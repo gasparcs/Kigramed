6bbfaf187891e100d1d2cfc72076ee3ca3e148b9
Chala4114 <chaladesousacopj@gmail.com>
2026-05-08 15:20:43 +0100

    Correções

--- src/Backend/K03.APPLICATION/SMSUseCase/Comand/AdicionarSMS.cs
+++ src/Backend/K03.APPLICATION/SMSUseCase/Comand/AdicionarSMS.cs
@@ -1,13 +1,11 @@
 using System;
-using Backend.K03.APPLICATION.ClienteUseCase.Queries;
-using Backend.K03.APPLICATION.FuncionarioUseCase.Queries;
 using Backend.K03.APPLICATION.SMSUseCase.DTO;
 using Backend.K04.DOMAIN.D20.SMS;
 using Backend.K04.DOMAIN.Interfaces;
 
 namespace Backend.K03.APPLICATION.SMSUseCase.Comand;
 
-public class AdicionarSMS(ICadastrarRepository<SMSModel> repository, PegarClientePeloNif clienterepository, PegarFuncionaarioPeloNif funcionariorepository)
+public class AdicionarSMS(ICadastrarRepository<SMSModel> repository)
 {
      public async Task<string> ExecuteAsync(AdicionarSMSDTO dto)
     {
--- src/Backend/obj/Debug/net10.0/Backend.AssemblyInfo.cs
+++ src/Backend/obj/Debug/net10.0/Backend.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Backend")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+88722ef034403e57cbfa86f719621735f52a449f")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+a20ce9ebb058b858804e08c6c4d373ed23656455")]
 [assembly: System.Reflection.AssemblyProductAttribute("Backend")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Backend")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]

c5b5adb5b2e240186be9b66794a079dd86efacc8
Chala4114 <chaladesousacopj@gmail.com>
2026-05-13 16:28:27 +0100

    implementação  do frontend externo

--- src/Backend/obj/Debug/net10.0/Backend.AssemblyInfo.cs
+++ src/Backend/obj/Debug/net10.0/Backend.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Backend")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+14b7e713c5684e0c83dd98f95fd1a3e9772a006e")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+ba4d11eb3ab5bb8c0363af7c564ecf8940422773")]
 [assembly: System.Reflection.AssemblyProductAttribute("Backend")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Backend")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]

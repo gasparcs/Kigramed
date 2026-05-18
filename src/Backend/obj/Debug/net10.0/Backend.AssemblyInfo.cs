6e96b5182b460b00f9ddf0c576eb770f4396ea76
gasparcs <gasparcastro.quindamba12@gmail.com>
2026-05-18 00:06:39 +0000

    reformulando o projecto

--- src/Backend/obj/Debug/net10.0/Backend.AssemblyInfo.cs
+++ src/Backend/obj/Debug/net10.0/Backend.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Backend")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+f366944fcbb30a65f85f494f902dd037403ef28a")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+aa9210a9d266a4b267cdf4126e1913049722eb44")]
 [assembly: System.Reflection.AssemblyProductAttribute("Backend")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Backend")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
